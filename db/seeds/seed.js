a4d5e44bda721229783ba994787e69cf310ce1c4
amy <amy@amy-thinkpad-t460s.mynet>
2021-08-03 16:00:52 +0100

    small refactor to layout

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -11,18 +11,24 @@ const seed = async (data) => {
   const { articleData, commentData, topicData, userData } = data;
   await db.query(`DROP TABLE IF EXISTS comments;`);
   await db.query(`DROP TABLE IF EXISTS articles;`);
-  await db.query(`DROP TABLE IF EXISTS users;`);
-  await db.query(`DROP TABLE IF EXISTS topics;`);
+  await Promise.all([
+    db.query(`DROP TABLE IF EXISTS users;`),
+    db.query(`DROP TABLE IF EXISTS topics;`),
+  ]);
   console.log("Dropped all tables");
-  await db.query(`CREATE TABLE topics (
-    slug VARCHAR(100) PRIMARY KEY,
-    description VARCHAR(300)
-  );`);
-  await db.query(`CREATE TABLE users (
-    username VARCHAR(100) PRIMARY KEY,
-    avatar_url VARCHAR(230),
-    name VARCHAR(100)
-  );`);
+
+  await Promise.all([
+    db.query(`CREATE TABLE topics (
+      slug VARCHAR(100) PRIMARY KEY,
+      description VARCHAR(300)
+    );`),
+    db.query(`CREATE TABLE users (
+      username VARCHAR(100) PRIMARY KEY,
+      avatar_url VARCHAR(230),
+      name VARCHAR(100)
+    );`),
+  ]);
+
   await db.query(`CREATE TABLE articles (
     article_id SERIAL PRIMARY KEY,
     title VARCHAR(150) NOT NULL,
@@ -32,6 +38,7 @@ const seed = async (data) => {
     author VARCHAR(100) REFERENCES users(username),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   );`);
+
   await db.query(`CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
     author VARCHAR(100) REFERENCES users(username),
@@ -43,35 +50,29 @@ const seed = async (data) => {
   console.log("created tables!");
 
   const topicInsertQuery = format(
-    `
-  INSERT INTO topics
+    `INSERT INTO topics
     (slug, description)
-  VALUES
-    %L;
-  `,
+    VALUES
+    %L;`,
     formatValues(topicData, ["slug", "description"])
   );
   await db.query(topicInsertQuery);
 
   const userInsertQuery = format(
-    `
-  INSERT INTO users
+    `INSERT INTO users
     (username, avatar_url, name)
-  VALUES
-    %L;
-  `,
+    VALUES
+    %L;`,
     formatValues(userData, ["username", "avatar_url", "name"])
   );
   await db.query(userInsertQuery);
 
   const articleInsertQuery = format(
-    `
-  INSERT INTO articles
+    `INSERT INTO articles
     (title, body, votes, topic, author, created_at)
-  VALUES
+    VALUES
     %L
-    RETURNING article_id, title;
-  `,
+    RETURNING article_id, title;`,
     formatValues(articleData, [
       "title",
       "body",
@@ -97,12 +98,10 @@ const seed = async (data) => {
   );
 
   const commentInsertQuery = format(
-    `
-  INSERT INTO comments
+    `INSERT INTO comments
     (author, article_id, votes, created_at, body)
-  VALUES
-    %L;
-  `,
+    VALUES
+    %L;`,
     formatValues(formattedCommentDataWithId, [
       "author",
       "article_id",
