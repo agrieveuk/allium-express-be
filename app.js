0f489bea2e69b788638a0b5f2cd64479e08599cd
amy <algrieve1@gmail.com>
2021-10-08 17:07:29 +0100

    change to double quotes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const {
   handle404s,
   handlePSQLErrors,
   handleCustomErrors,
-  handle500s,
-} = require('./errors');
-const apiRouter = require('./routers/api.router');
-const cors = require('cors');
+  handle500s
+} = require("./errors");
+const apiRouter = require("./routers/api.router");
+const cors = require("cors");
 
 const app = express();
 
@@ -14,9 +14,9 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-app.use('*', handle404s);
+app.use("*", handle404s);
 
 app.use(handlePSQLErrors);
 app.use(handleCustomErrors);
